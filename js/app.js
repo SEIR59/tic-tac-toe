b2f706127f4cb6fdfd77c8666da177e177ddee21
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-13 22:45:07 -0700

    Correctly identifies winner, trouble removing listeners and pausing game at end

--- js/app.js
+++ js/app.js
@@ -1,45 +1,117 @@
+// #region functions - collapse this to line to jump to code.
 
-//initialize button(s) and listeners
-document.getElementById('resetbtn').addEventListener('click', function() {clearMoves()})
-
-const allSquares = document.getElementsByClassName('square')
-for(let i = 0; i < allSquares.length;i++){
-    allSquares[i].addEventListener('click', ()=>{
-        playerMove(event.target)
-        console.log('click')
-    })
-}
-
-//track turns of players and their movements
-let currentTurn = 0
-let p1 = []
-let p2 = []
-
-//function to do things when a player makes a move
-const playerMove = (location) =>{
+//function to do things when a player makes a move. IDs are structured as codes to win conditions. ID[0:2]-> verticles, ID[3:5]->horazontals, ID[6:7]->diagonals. Contains '1' in position if it helps that win condition. Sum of 3 means 3 in a row, which = win.
+//ID example X chooses 3 squares. ID[6:7] for the divs IDs are top left '10', middle '11, bottom right '10'. ID[6] === 3, which is a win, diagonal.
+const playerMove = (location) => {
+    let id = location.id.split('').map(function (x) { return Number(x) })
+    let title = document.getElementById('title')
     //console.log('in playerMove')
-    if(currentTurn === 0 && location.innerText===''){
+    if (currentTurn === 0 && location.innerText === '') {
         //things for player 1
-        currentTurn = 1
         location.innerText = 'X'
-    }else if (currentTurn === 1 && location.innerText===''){
+        tieCheck += id.reduce((x, y) => x + y, 0)
+        currentTurn = 1
+        title.innerText = `O's turn`
+        //digest the event id and add it to player array to check for wins.
+        for (let i = 0; i < id.length; i++) {
+            X[i] += id[i];
+            if (X[i] === 3) {
+                winnerFound('X')
+                return
+            }
+        }
+        if (tieCheck === 24) {
+            winnerFound()
+            return
+        }
+        //console.log('X ' + X)
+    } else if (currentTurn === 1 && location.innerText === '') {
         //things for player 2
-            location.innerText = 'O' 
-            currentTurn = 0  
+        location.innerText = 'O'
+        currentTurn = 0
+        tieCheck += id.reduce((x, y) => x + y, 0)
+        title.innerText = `X's turn`
+        for (let i = 0; i < id.length; i++) {
+            O[i] += id[i];
+            if (O[i] === 3) {
+                winnerFound('O')
+                return
+            }
+        }
+        if (tieCheck === 24) {
+            winnerFound()
+            return
+        }
+        //console.log('O ' + O)
     }
+    //check for ties
+    //console.log('tc current' + tieCheck)
+
 }
 
 //function to see if somebody won
-const checkForWins = () =>{
-    //console.log('in checkForWins')
+const winnerFound = (winner) => {
+    if (winner === 'X' || winner === 'O') {
+        console.log(`${winner}'s win!`)
+        document.getElementById('title').innerText = `${winner} wins!`
+    } else {
+        console.log('Tie')
+        document.getElementById('title').innerText = `Booo. Tie.`
+    }
+    boardClickOn(false)
+    for (let i = 0; i < allSquares.length; i++) {
+        allSquares[i].style.background = 'purple'
+    }
 }
 
 //function to reset the gameboard
-const clearMoves = () =>{
+const clearMoves = () => {
     //console.log('in clearMoves')
-    for(let i = 0; i<allSquares.length;i++){
+    for (let i = 0; i < allSquares.length; i++) {
         allSquares[i].innerText = '';
     }
-    p1 = []
-    p2 = []
+    X = [0, 0, 0, 0, 0, 0, 0, 0]
+    O = [0, 0, 0, 0, 0, 0, 0, 0]
+    currentTurn = 0;
+    boardClickOn(true)
+    for (let i = 0; i < allSquares.length; i++) {
+        allSquares[i].style.background = 'white'
+    }
+    document.getElementById('title').innerText = `Let's play some TTT`
+    tieCheck = 0;
 }
+
+//toggle board click listening. true = on, false = off.
+const boardClickOn = (bool) => {
+
+    const playerMoveBridge = () => {
+        const target = event.target
+        playerMove(target)
+    }
+
+    if (bool === true) {
+        for (let i = 0; i < allSquares.length; i++) {
+            allSquares[i].addEventListener('click', playerMoveBridge)
+        }
+
+    } /* else {
+        for (let i = 0; i < allSquares.length; i++) {
+            console.log('remove tried')
+            $(allSquares[i]).off();
+        }
+    } */
+}
+
+// #endregion 
+
+
+//initialize button(s) and listeners
+document.getElementById('resetbtn').addEventListener('click', function () { clearMoves() })
+const allSquares = document.getElementsByClassName('square') //global variable for all the game squares.
+boardClickOn(true)
+
+//track turns of players and their movements
+let currentTurn = 0
+let X = [0, 0, 0, 0, 0, 0, 0, 0]
+let O = [0, 0, 0, 0, 0, 0, 0, 0]
+let tieCheck = 0;
