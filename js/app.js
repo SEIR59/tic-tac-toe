7f178b7bee4f07f182b184c8e694a8dce66ccddb
bhenderson93 <brycehenderson1993@gmail.com>
2022-05-14 12:29:02 -0700

    Slider buttons working, now on to PvComputer options

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-// #region functions - collapse this to line to jump to code.
+// #region Functions - collapse this to line to jump to code.
 
-//function to do things when a player makes a move. IDs are structured as codes to win conditions. ID[0:2]-> verticles, ID[3:5]->horazontals, ID[6:7]->diagonals. Contains '1' in position if it helps that win condition. Sum of 3 means 3 in a row, which = win.
-//ID example X chooses 3 squares. ID[6:7] for the divs IDs are top left '10', middle '11, bottom right '10'. ID[6] === 3, which is a win, diagonal.
+//function to do things when a player makes a move. 
+//IDs are structured as codes to win conditions. ID[0:2]-> verticles, ID[3:5]->horazontals, ID[6:7]->diagonals. Contains '1' in position if it helps that win condition. Sum of 3 in an array position means 3 connected, which = win.
+//ID example: X chooses top left, middle, and bottom right. ID[6:7] for the divs are top left '10', middle '11, bottom right '10'. After all moves, X[6] === 3, which is a win, diagonal.
 const playerMove = (location) => {
     let id = location.id.split('').map(function (x) { return Number(x) })
     let title = document.getElementById('title')
@@ -9,7 +10,7 @@ const playerMove = (location) => {
     if (currentTurn === 0 && location.innerText === '') {
         //things for player 1
         location.innerText = 'X'
-        tieCheck += id.reduce((x, y) => x + y, 0)
+        tieCheck += 1;
         currentTurn = 1
         title.innerText = `O's turn`
         //digest the event id and add it to player array to check for wins.
@@ -20,7 +21,7 @@ const playerMove = (location) => {
                 return
             }
         }
-        if (tieCheck === 24) {
+        if (tieCheck === 9) {
             winnerFound()
             return
         }
@@ -29,7 +30,7 @@ const playerMove = (location) => {
         //things for player 2
         location.innerText = 'O'
         currentTurn = 0
-        tieCheck += id.reduce((x, y) => x + y, 0)
+        tieCheck += 1
         title.innerText = `X's turn`
         for (let i = 0; i < id.length; i++) {
             O[i] += id[i];
@@ -37,10 +38,6 @@ const playerMove = (location) => {
                 winnerFound('O')
                 return
             }
-        }
-        if (tieCheck === 24) {
-            winnerFound()
-            return
         }
         //console.log('O ' + O)
     }
@@ -72,11 +69,13 @@ const winnerFound = (winner) => {
     updateScoreboard()
 }
 
+//function to update the text of the scoreboard with the new score values
 const updateScoreboard = () =>{
     document.getElementById('sbX').innerText = `Player X: ${currentScore[0]}`
     document.getElementById('sbO').innerText = `Player O: ${currentScore[1]}`
     document.getElementById('sbT').innerText = `Ties: ${currentScore[2]}`
 }
+
 //function to reset the gameboard
 const clearMoves = () => {
     //console.log('in clearMoves')
@@ -107,16 +106,51 @@ const boardClickOn = (bool) => {
         $('#gameboard').off()
         }
     }
+//function to shift the clicked slider button right and all others left.
+const moveSliderButton = () =>{
+    const target = event.target.id
+    //console.log(target)
 
+    const toggleDirection = (id)=>{
+        let ele = document.getElementById(id)
+        let style = getComputedStyle(ele).justifyContent
+        let btns = document.getElementsByClassName('slider')
+        if (style === 'left'){
+            for(let i = 0; i<btns.length;i++){
+                btns[i].style.justifyContent = 'left'
+            }
+            ele.style.justifyContent = 'right'
+        }
+    }
+
+    if(target === 'PvPbtn' || target === 'PvPSlider'){
+        toggleDirection('PvPSlider')
+    }else if(target === 'PvCbtn'|| target === 'PvCSlider'){
+        toggleDirection('PvCSlider')
+    }else if(target === 'PvAbtn'|| target === 'PvASlider'){
+        toggleDirection('PvASlider')
+    }
+}
+
+//ran on game startup. Start listeners and input default settings.
+const initializeGame = ()=>{
+    document.getElementById('PvPSlider').style.justifyContent = 'right'
+    $('#resetbtn').on('click', clearMoves)
+    $('#gameOptions').on('click' , moveSliderButton)
+    boardClickOn(true)
+}
 // #endregion 
-//initialize button(s) and listeners
-document.getElementById('resetbtn').addEventListener('click', function () { clearMoves() })
-const allSquares = document.getElementsByClassName('square') //global variable for all the game squares.
-boardClickOn(true)
+//#region Initialize button(s) and listeners
 
-//track turns of players and their movements
+const allSquares = document.getElementsByClassName('square') //global variable for all the game squares.
+initializeGame()
+
+// #endregion
+
+//#region Variables to track turns of players and their movements
 let currentTurn = 0
 let X = [0, 0, 0, 0, 0, 0, 0, 0]
 let O = [0, 0, 0, 0, 0, 0, 0, 0]
 let tieCheck = 0;
 const currentScore = [0,0,0]
+//#endregion 
