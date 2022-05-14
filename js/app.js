b444a8b2112209380c030bbb96f0709fa251bb3a
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-14 16:18:32 -0400

    Fixed all my functionality issues. AI still doesn't work. Get to that later.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 const cells = document.querySelectorAll(".cell");
+const cellsarr = Array.from(cells)
 const restartBtn = document.querySelector(".restart-btn");
 const nxtUp = document.querySelector(".next-up");
 let letter = 1;
 let computer = false;
 let board = [];
+const playHum = document.querySelector(".play-hum");
 const playComp = document.querySelector(".play-comp");
+let possiblePicks = [0,1,2,3,4,5,6,7,8]
+
+console.log(cellsarr)
 
 //creates board at start of game
 for (let i = 0; i < cells.length; i++) {
@@ -17,8 +22,6 @@ function updateBoard() {
   }
 }
 
-playHuman();
-
 //Update Next Up Element
 function displayNextUpMessage(num) {
   if (num === 0) {
@@ -28,59 +31,83 @@ function displayNextUpMessage(num) {
   }
 }
 
-// //Set Computer to true/false
+//Start game vs Human
+playHum.addEventListener("click", function(){
+  play(computer);
+  playHum.innerHTML = "You are currently playing another Human"
+})
+
+//Set Computer to true and run play computer
 // playComp.addEventListener("click", function () {
-//     restart();
-//     //console.log(computer)
-//     if (playComp.innerHTML === "Play Computer") {
-//         computer = true;
-//         playComp.innerHTML = "Go back to playing humans!";
-//         play(computer);
-//     } else {
-//         computer = false;
-//         playComp.innerHTML = "Play Computer";
-//         //console.log(playComp.innerHTML)
-//         //play(computer);
-//     }
+//   computer = true;
+//   playComp.innerHTML = "You are currently playing Computer"
+//   play(computer);
 // });
 
-// function play(a){
-//     if (a){
-//         playHuman()
-//         console.log("we are playing computer")
-//     } else {
-//         playHuman()
-//         console.log("we are playing humans")
-//         //console.log(letter)
-//     }
-// }
+function play(a){
+    if (a){
+        playComputer();
+        console.log("we are playing computer")
+    } else {
+        playHuman()
+        console.log("we are playing humans")
+        //console.log(letter)
+    }
+}
 
 //Allows user to click cell and leave either an x or an o
 function playHuman() {
   for (const cell of cells) {
     cell.addEventListener("click", function (e) {
       //console.log(e.target.classList)
-      console.log(board);
-      checkforWin();
+      //console.log(`this is the board before move: ${board}`);
       isMovesLeft();
       //isMovesLeft();
       if (letter === 1) {
         cell.innerHTML = "X";
-        letter = letter - 1;
+        letter = 0;
         //console.log(letter)
         board = [];
         updateBoard();
+        checkforWin();
+        console.log(board)
       } else {
         cell.innerHTML = "O";
-        letter = letter + 1;
-        //console.log(letter);
+        letter = 1;
+        console.log(letter);
         board = [];
         updateBoard();
+        checkforWin();
+        console.log(board)
       }
       //console.log(letter)
       displayNextUpMessage(letter);
     });
   }
+}
+
+function playComputer(){
+    //pickedCell = Math.floor(Math.random() * board.length);
+    for (const cell of cells){
+        cell.addEventListener("click", function(){
+            isMovesLeft();
+            if(letter === 1){
+                cell.innerHTML = "X";
+                board = [];
+                //updateBoard();
+                //console.log(board);
+                console.log(possiblePicks.splice(Math.floor(Math.random() * 9)))
+                // compPickedCell = Math.floor(Math.random() * 9);
+                //console.log(compPickedCell)
+                //board[compPickedCell] = "O"
+                console.log(board)
+                //cellsarr[possiblePicks.splice(Math.floor(Math.random() * possiblePicks.length))].innerHTML = "O"
+                updateBoard();
+                checkforWin();
+            }
+        })
+    }
+  
 }
 
 function checkforWin() {
@@ -161,6 +188,8 @@ function restart() {
   });
   letter = 1;
   nxtUp.innerHTML = "Next up: X";
+  playHum.innerHTML = "Play Human"
+  playComp.innerHTML = "Play Computer"
 }
 //checks for if movesLeft
 function isMovesLeft() {
