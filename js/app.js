ee417520e19a9a56caf7d9ee93ac03d41583fa8c
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-13 21:57:47 -0400

    Created Grid, Restart button(No functionality), user click cell

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('Hello frontend');
+const cells = document.querySelectorAll('.cell')
+
+//Allows user to click cell and leave either an x or an o
+let letter = 1;
+for (const cell of cells){
+    cell.addEventListener('click', function(e){
+        if(letter === 1){
+            cell.innerHTML = "X";
+            letter = letter - 1;
+        } else {
+            cell.innerHTML = "O"
+            letter = letter + 1
+            console.log("add letter")
+        }
+    })
+}
