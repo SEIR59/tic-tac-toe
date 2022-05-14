85e3de0b88d7f8355070fa48dce908b0c6588476
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-14 09:12:10 -0400

    added play computer button, slight functionality, changed restart to a function

--- js/app.js
+++ js/app.js
@@ -1,38 +1,58 @@
-const cells = document.querySelectorAll('.cell')
-const restart = document.querySelector('.restart-btn')
-const nxtUp = document.querySelector('.next-up')
+const cells = document.querySelectorAll(".cell");
+const restartBtn = document.querySelector(".restart-btn");
+const nxtUp = document.querySelector(".next-up");
+let letter = 1;
+let computer = false;
+const playComp = document.querySelector(".play-comp");
 
 //Allows user to click cell and leave either an x or an o
-let letter = 1;
-for (const cell of cells){
-    cell.addEventListener('click', function(){
-        if(letter === 1){
-            cell.innerHTML = "X";
-            letter = letter - 1;
-            console.log(letter)
-        } else {
-            cell.innerHTML = "O"
-            letter = letter + 1
-            console.log(letter)
-        }
-    displayNextUpMessage(letter)
-    })
+function playHuman() {
+  for (const cell of cells) {
+    cell.addEventListener("click", function () {
+      if (letter === 1) {
+        cell.innerHTML = "X";
+        letter = letter - 1;
+        // console.log(letter)
+      } else {
+        cell.innerHTML = "O";
+        letter = letter + 1;
+        // console.log(letter)
+      }
+      displayNextUpMessage(letter);
+    });
+  }
+}
+playHuman();
+
+//Update Next Up Element
+function displayNextUpMessage(num) {
+  if (num === 0) {
+    nxtUp.innerHTML = "Next up: O";
+  } else {
+    nxtUp.innerHTML = "Next up: X";
+  }
 }
 
-//Restart Button
-restart.addEventListener('click',function(){
-    cells.forEach(function(cell){
-        cell.innerHTML = ""
-    })
-    letter = 1;
-    nxtUp.innerHTML = "Next up: X"
+//Set Computer to true/false
+playComp.addEventListener("click", function () {
+  if (playComp.innerHTML === "Play Computer") {
+    computer = true;
+    playComp.innerHTML = "Go back to playing humans!";
+  }
+});
+
+
+//Restart Btn
+restartBtn.addEventListener('click', function(){
+    restart();
 })
 
-function displayNextUpMessage(num){
-    if(num === 0){
-        nxtUp.innerHTML = "Next up: O"
-    } else {
-        nxtUp.innerHTML = "Next up: X"
-    }
+//Restart Function
+function restart() {
+    cells.forEach(function (cell) {
+      cell.innerHTML = "";
+    });
+    letter = 1;
+    nxtUp.innerHTML = "Next up: X";
 }
 
