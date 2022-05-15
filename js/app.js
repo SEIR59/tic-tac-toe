5e77b38c3cb07a6b4ed98a1e6eb8a692df2f0761
earlhalasan <earl.halasan@gmail.com>
2022-05-15 15:49:30 -0400

    Cleaned js file to be more organized

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-let playBoard = document.querySelector(".square");
+let gameBoard = document.querySelector(".square");
 
-playBoard.addEventListener("click", () => {
-  document.getElementById("box1").innerHTML = "X";
+let squareArr = ["", "", "", "", "", "", "", "", ""];
+let firstPlayer = "X";
+let secondPlayer = "O";
+let turn = 0;
+let lastClicked;
+
+// length of turns
+for (i = 0; i <= 9; i++) {}
+
+document.addEventListener("click", () => {
+  document.getElementById("box1").innerHTML = firstPlayer;
 });
 
-playBoard.addEventListener("click", () => {
-  document.getElementById("box2").innerHTML = "O";
+document.addEventListener("click", () => {
+  document.getElementById("box2").innerHTML = secondPlayer;
 });
+
+const winCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
