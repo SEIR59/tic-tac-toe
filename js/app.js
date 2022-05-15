4128748f21d69ced5849f3e72ff56a94e1b9f187
Jason Lull <jlull7774@gmail.com>
2022-05-15 14:02:34 -0400

    domcontent

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+window.addEventListener('DOMConteentLoaded', () => {
+    const resetButton = document.querySelector('input');
+    const ticTacToeBoxes = Array.from(document.querySelectorAll('.box'))
+
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    let player1 = "X";
+    
+}
 
