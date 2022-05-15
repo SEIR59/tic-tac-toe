999e799872529d7f8de81e3eeed4a636dcf748ae
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-05-15 10:39:41 -0400

    got display win condition js working properly

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ const score1 = document.querySelector(".scoreX")
 const score2 = document.querySelector(".scoreO")
 let s1 = 0;
 let s2 = 0
+let winner = ""
+let winarr =[]
 
 //creates board at start of game
 for (let i = 0; i < cells.length; i++) {
@@ -60,7 +62,7 @@ function play(a){
 //Allows user to click cell and leave either an x or an o
 function playHuman() {
   for (const cell of cells) {
-    console.log(cell.classList)
+    //console.log(cell.classList)
     cell.addEventListener("click", function (e) {
       //console.log(e.target.classList)
       //console.log(`this is the board before move: ${board}`);
@@ -75,7 +77,6 @@ function playHuman() {
           addPickedClass(cell)
           setUser(cell)
           updateBoard();
-          checkforWin();
           console.log(cell.classList)
           console.log(board)
         } else {
@@ -86,13 +87,14 @@ function playHuman() {
           addPickedClass(cell)
           setComp(cell)
           updateBoard();
-          checkforWin();
           console.log(cell.classList)
           console.log(board)
         }
       }
       //console.log(letter)
       displayNextUpMessage(letter);
+      checkforWin();
+      console.log(winarr[0])
     });
   }
 }
@@ -107,6 +109,7 @@ function playComputer(){
                   cell.innerHTML = "X";
                   board = [];
                   addPickedClass(cell);
+                  setUser(cell)
                   //console.log(board)
                   let compPickCell = 0
                   function randomNumAndSet(){
@@ -130,114 +133,222 @@ function playComputer(){
                       //console.log("not getting classList")
                     }
                   }
-                  updateBoard();
-                  console.log(cellsarr)
                 }
               } 
+              updateBoard();
+              //console.log(cellsarr)
               checkforWin();
+              console.log(winarr[0])
             })
-      }
+          }
 }
 function checkforWin() {
-  let winner = ""
-  for (let i = 0; i < board.length; i++) {
+  console.log("checkforWinRan")
+  winarr = []
+  for (let i = 0; i < cellsarr.length; i++) {
     //check rows
-    if (board[0] === board[1] && board[0] === board[2]) {
-      if (board[0] === "X") {
+    if ((cells[0].classList.contains("user") && cells[1].classList.contains("user") && cells[2].classList.contains("user")) || (cells[0].classList.contains("comp") && cells[1].classList.contains("comp") && cells[2].classList.contains("comp"))) {
+      if (cells[0].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[0] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
-    else if (board[3] === board[4] && board[3] === board[5]) {
-      if (board[3] === "X") {
+    else if ((cells[3].classList.contains("user") && cells[4].classList.contains("user") && cells[5].classList.contains("user")) || (cells[3].classList.contains("comp") && cells[4].classList.contains("comp") && cells[5].classList.contains("comp"))) {
+      if (cells[3].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[3] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
-    if (board[6] === board[7] && board[6] === board[8]) {
-      if (board[6] === "X") {
+    if ((cells[6].classList.contains("user") && cells[7].classList.contains("user") && cells[8].classList.contains("user")) || (cells[6].classList.contains("comp") && cells[7].classList.contains("comp") && cells[8].classList.contains("comp"))) {
+      if (cells[6].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[6] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
     //check columns
-    else if (board[0] === board[3] && board[0] === board[6]) {
-      if (board[0] === "X") {
+    else if ((cells[0].classList.contains("user") && cells[3].classList.contains("user") && cells[6].classList.contains("user")) || (cells[0].classList.contains("comp") && cells[3].classList.contains("comp") && cells[6].classList.contains("comp"))) {
+      if (cells[0].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[0] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
-    else if (board[1] === board[4] && board[1] === board[7]) {
-      if (board[1] === "X") {
+    else if ((cells[1].classList.contains("user") && cells[4].classList.contains("user") && cells[7].classList.contains("user")) || (cells[1].classList.contains("comp") && cells[4].classList.contains("comp") && cells[7].classList.contains("comp"))) {
+      if (celss[1].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[1] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
-    else if (board[2] === board[5] && board[2] === board[8]) {
-      if (board[2] === "X") {
+    else if ((cells[2].classList.contains("user") && cells[5].classList.contains("user") && cells[8].classList.contains("user")) || (cells[2].classList.contains("comp") && cells[5].classList.contains("comp") && cells[8].classList.contains("comp"))) {
+      if (cells[2].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[2] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
     //check diagonalls
-    else if (board[0] === board[4] && board[0] === board[8]) {
-      if (board[0] === "X") {
+    else if ((cells[0].classList.contains("user") && cells[4].classList.contains("user") && cells[8].classList.contains("user")) || (cells[0].classList.contains("comp") && cells[4].classList.contains("comp") && cells[8].classList.contains("comp"))) {
+      if (cells[0].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[0] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
         addScore(score2)
       }
+      //gameIsOver();
     }
-    else if (board[2] === board[4] && board[2] === board[6]) {
-      if (board[2] === "X") {
+    else if ((cells[2].classList.contains("user") && cells[4].classList.contains("user") && cells[6].classList.contains("user")) || (cells[2].classList.contains("comp") && cells[4].classList.contains("comp") && cells[6].classList.contains("comp"))) {
+      if (cells[2].classList.contains("user")) {
         gameIsOver();
         winner = "X"
         addScore(score1)
-      } else if (board[2] === "O") {
+      } else {
         gameIsOver();
         winner = "O"
       }
     } else if(!isMovesLeft()){
-        gameIsOver();
+        gameIsOver()
         winner = "It was a tie!"
         addScore(score2)
     }
+    //gameIsOver();
   }
-  console.log(`${winner} was the winner!`)
 }
+// function checkforWin() {
+//   for (let i = 0; i < board.length; i++) {
+//     //check rows
+//     if (board[0] === board[1] && board[0] === board[2]) {
+//       if (board[0] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[0] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     else if (board[3] === board[4] && board[3] === board[5]) {
+//       if (board[3] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[3] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     if (board[6] === board[7] && board[6] === board[8]) {
+//       if (board[6] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[6] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     //check columns
+//     else if (board[0] === board[3] && board[0] === board[6]) {
+//       if (board[0] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[0] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     else if (board[1] === board[4] && board[1] === board[7]) {
+//       if (board[1] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[1] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     else if (board[2] === board[5] && board[2] === board[8]) {
+//       if (board[2] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[2] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     //check diagonalls
+//     else if (board[0] === board[4] && board[0] === board[8]) {
+//       if (board[0] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[0] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//         addScore(score2)
+//       }
+//     }
+//     else if (board[2] === board[4] && board[2] === board[6]) {
+//       if (board[2] === "X") {
+//         gameIsOver();
+//         winner = "X"
+//         addScore(score1)
+//       } else if (board[2] === "O") {
+//         gameIsOver();
+//         winner = "O"
+//       }
+//     } else if(!isMovesLeft()){
+//         gameIsOver();
+//         winner = "It was a tie!"
+//         addScore(score2)
+//     }
+//   }
+//   console.log(`${winner} was the winner!`)
+// }
 
 //Restart Btn
 restartBtn.addEventListener("click", function () {
@@ -250,6 +361,7 @@ function restart() {
     cell.innerHTML = "";
   });
   letter = 1;
+  winner = ""
   nxtUp.innerHTML = "Next up: X";
   playHum.innerHTML = "Play Human"
   playComp.innerHTML = "Play Computer"
@@ -276,7 +388,9 @@ function gameIsOver(){
       addPickedClass(cell)
       //console.log(cell.classList)
     }
-    console.log("The game is over")
+    //console.log("The game is over")
+    winarr.push("the game is overrr. How many times does this print?")
+    //console.log(`${winner} was the winner!`)
   }
 }
 
@@ -295,6 +409,7 @@ function addScore(el){
     el.innerHTML = s2;
   }
   console.log(s1)
+  console.log(s2)
 }
 
 //create and add user class
