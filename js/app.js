d895bae342389b7df03aadc9d480e42ae53206e9
andersons1218 <andersons1218@gmail.com>
2022-05-15 17:24:13 -0400

    tic-tac-toe update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('Hello frontend');
+// this is where i start my js. first i want to try to do something when i click on a box.
+const boxes= document.querySelectorAll(".box");
+const board = document.querySelector('.board');
+const button = document.querySelector('.button');
+
+let user = 'x'
+for (const box of boxes){
+    box.addEventListener("click", function(){
+        box.innerHTML = user;
+        if ( user === x );
+        else( user = o);
+        box.innerHTML = o
+    }) 
+    
+}
+
+
