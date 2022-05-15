21985bcd1d9e43a69f5dd65766ec6b4a73244fde
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-05-15 17:31:14 -0400

    Updated game to stop taking click inputs after any win condition has been reached

--- js/app.js
+++ js/app.js
@@ -11,8 +11,9 @@ let p1 = " ";
 let p2 = " ";
 let p1Wins = 0;
 let p2Wins = 0;
+let turnsTaken = 0;
 let turn = 0;
-let board = document.getElementById("board");
+let gameOver = false;
 // let one = document.getElementById("one");
 // let two = document.getElementById("two");
 // let three = document.getElementById("three");
@@ -56,6 +57,7 @@ function gameOn() {
     oButton.style.display = "none";
     topPrompt.innerHTML = `Game on! You are ${p1}`
     bottomPrompt.style.display = "inline";
+    restartButton.style.display = "inline";
     p1Turn();
 }
 
@@ -72,27 +74,30 @@ function p2Turn() {
 const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => {
     box.addEventListener('click', function handleClick(event) {
-        let currentBox = this.id;
-        console.log(currentBox); // Test log ?
-        let currentValue = document.getElementById(currentBox).innerHTML;
-        console.log(`Current value = ${currentValue}`)
-        if (turn === 1) {
-            if (currentValue !== p2) {
-                console.log("empty!") // Test log;
-                document.getElementById(currentBox).innerHTML = p1;
-                document.getElementById(currentBox).style.color = "black";
-                checkForWin(p1);
-                p2Turn();
+        if ((turnsTaken !== 9) && (gameOver === false)) {
+            let currentBox = this.id;
+            console.log(currentBox); // Test log ?
+            let currentValue = document.getElementById(currentBox).innerHTML;
+            console.log(`Current value = ${currentValue}`)
+            if (turn === 1) {
+                if (currentValue !== p2) {
+                    console.log("empty!") // Test log;
+                    document.getElementById(currentBox).innerHTML = p1;
+                    document.getElementById(currentBox).style.color = "black";
+                    p2Turn();
+                    checkForWin(p1);
+                }
             }
-        }
-        else if (turn === 2) {
-            if (currentValue !== p1) {
-                console.log("empty!") // Test log;
-                document.getElementById(currentBox).innerHTML = p2;
-                document.getElementById(currentBox).style.color = "black";
-                checkForWin(p2);
-                p1Turn();
+            else if (turn === 2) {
+                if (currentValue !== p1) {
+                    console.log("empty!") // Test log;
+                    document.getElementById(currentBox).innerHTML = p2;
+                    document.getElementById(currentBox).style.color = "black";
+                    p1Turn();
+                    checkForWin(p2);
+                }
             }
+            turnsTaken++;
         }
     })
 });
@@ -100,32 +105,35 @@ boxes.forEach(box => {
 function checkForWin(p) {
     console.log(`checking p for ${p}`);
     if (((one.innerHTML === p) && (two.innerHTML === p) && (three.innerHTML === p)) ||
-    ((four.innerHTML === p) && (five.innerHTML === p) && (six.innerHTML === p)) ||
-    ((seven.innerHTML === p) && (eight.innerHTML === p) && (nine.innerHTML === p)) ||
-    ((one.innerHTML === p) && (four.innerHTML === p) && (seven.innerHTML === p)) ||
-    ((two.innerHTML === p) && (five.innerHTML === p) && (eight.innerHTML === p)) ||
-    ((three.innerHTML === p) && (six.innerHTML === p) && (nine.innerHTML === p)) ||
-    ((one.innerHTML === p) && (five.innerHTML === p) && (nine.innerHTML === p)) ||
-    ((seven.innerHTML === p) && (five.innerHTML === p) && (three.innerHTML === p))) 
-    {
+        ((four.innerHTML === p) && (five.innerHTML === p) && (six.innerHTML === p)) ||
+        ((seven.innerHTML === p) && (eight.innerHTML === p) && (nine.innerHTML === p)) ||
+        ((one.innerHTML === p) && (four.innerHTML === p) && (seven.innerHTML === p)) ||
+        ((two.innerHTML === p) && (five.innerHTML === p) && (eight.innerHTML === p)) ||
+        ((three.innerHTML === p) && (six.innerHTML === p) && (nine.innerHTML === p)) ||
+        ((one.innerHTML === p) && (five.innerHTML === p) && (nine.innerHTML === p)) ||
+        ((seven.innerHTML === p) && (five.innerHTML === p) && (three.innerHTML === p))) {
         console.log("WIN!");
         if (p1 === p) {
             console.log("P1 WINS!");
+            bottomPrompt.innerHTML = "P1 WINS!";
+            gameOver = true;
             p1Wins++;
         }
         else if (p2 === p) {
             console.log("P2 WINS!");
+            bottomPrompt.innerHTML = "P2 WINS";
+            gameOver = true;
             p2Wins++;
+        }
+        else if (turnsTaken === 8) {
+            console.log("IT'S A TIE!");
+            bottomPrompt.innerHTML = "IT'S A TIE!";
+            gameOver = true;
         }
         scoreboard.innerHTML = `P1 ${p1Wins} - ${p2Wins} P2`;
     }
-    // ||
-    // ((four.innerHTML && five.innerHTML && six.innerHTML) === p) ||
-    // ((seven.innerHTML && eight.innerHTML && nine.innerHTML) === p) ||
-    // ((one.innerHTML && four.innerHTML && seven.innerHTML) === p) ||
-    // ((two.innerHTML && five.innerHTML && eight.innerHTML) === p) ||
-    // ((three.innerHTML && six.innerHTML && nine.innerHTML) === p) ||
-    // ((one.innerHTML && five.innerHTML && nine.innerHTML) === p) ||
-    // ((seven.innerHTML && five.innerHTML && three.innerHTML) === p)) 
+}
 
+function restart() {
+    
 }
