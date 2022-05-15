a93495fdc940c4fe7705572b4ac8d34fb28efc3f
Hoskyfull <norberto.cardenales@gmail.com>
2022-05-15 15:47:42 -0600

    made boxes adjust to page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 const restartBtn = document.getElementsByClassName('button')
-const box = document.getElementsByClassName('box')
+const box = Array.from(document.getElementsByClassName('box'))
  console.log (box)
+
+ const O_TEXT = "O"
+ const X_TEXT = "X"
+ let currentPlayer = X_TEXT
+ 
