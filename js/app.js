ca0a9765ffddf6dc9ccbe0c259a197cd459f8c88
“kmcaloney15” <Katherine@Katys-MBP-2.hsd1.ma.comcast.net>
2022-05-15 19:02:15 -0400

    Finally got the x's to appear when clicked

--- js/app.js
+++ js/app.js
@@ -1,53 +1,54 @@
 
-const gameBoardElement = document.querySelector('gameBoard')
+const gameBoardElement = document.querySelector('gameBoard');
 const boxes = document.querySelectorAll('.square');
 //Re-start button I can't get to work.
 // const restartButton = document.querySelector('#restart');
-const winningMessageText = document.querySelector('winningMessage')
+const resultText = document.querySelector('result');
 
 //declaring the player and computer and winning combinations
-const playerX = 'X';
-const playerO = 'O';
-const winningCombinations = [
-    //rows
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    //columns
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    //diagonals
-    [0,4,8],
-    [2,4,6],
-];
-//create game board into array
-let cells = ['', '', '', '', '', '', '', '', '',];
-
+// const playerX = 'X';
+// const playerO = 'O';
 // forces player x to always start
-let playerOTurn = false;
+let currentPlayer = 'X';
+// const winningCombinations = [
+//     //rows
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     //columns
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     //diagonals
+//     [0,4,8],
+//     [2,4,6],
+// ];
+// //create game board into array
+// let boxesArray = ['', '', '', '', '', '', '', '', '',];
 
 //to start the game
 //if it's a tie
 //to end the game
 
-
-
 //to start the game & add the click element to each box
 
-for (const box of boxes){
-    box.addEventListener("click", function(){
-        box.innerHTML = user;
-        if ( currentPlayer === x )
-
-    })
+for (const square of boxes) {
+   square.addEventListener("click", function(){
+       square.innerHTML = currentPlayer;
+   })
 }
+
+
+
+
+
+
 
 
 //how to switch turns
-function swapTurns(){
-    playerOTurn = !playerOTurn
-}
+// function swapTurns(){
+//     playerOTurn = !playerOTurn
+// }
 
 
 
