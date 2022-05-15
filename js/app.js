cc65ca13eb0d25b316dd3d8342e867061be5fe01
Jason Lull <jlull7774@gmail.com>
2022-05-15 14:13:30 -0400

    conditions

--- js/app.js
+++ js/app.js
@@ -10,7 +10,24 @@ window.addEventListener('DOMConteentLoaded', () => {
     const player2Score = "Player 2 Won";
     const gameCat = "Cat!";
 
+    let gameWinConditions = [
+        [0, 1, 2]
+        [3, 4, 5]
+        [6, 7, 8]
+        [0, 4, 8]
+        [0, 3, 6]
+        [1, 4, 7]
+        [2, 5, 8]
+        [2, 4, 6]
+        
+    ]
 
 
 
-}
+
+
+
+
+
+
+})
