1f33811a9af940ce8347490d1b6acaa296b575c1
Jason Lull <jlull7774@gmail.com>
2022-05-15 14:04:41 -0400

    players added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ window.addEventListener('DOMConteentLoaded', () => {
 
     let gameBoard = ["", "", "", "", "", "", "", "", ""];
     let player1 = "X";
+    const player2 = "O";
     
-}
+    const player1Score = "Player 1 Won";
+    const player2Score = "Player 2 Won";
+    const gameCat = "Cat!";
 
+
+
+
+}
