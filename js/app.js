6a753b35a26f1ad8ee0cc67e184360c5e9b6c9ef
Hoskyfull <norberto.cardenales@gmail.com>
2022-05-15 14:02:40 -0600

    adding variables to js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-const board;
-const player0 = "O";
-const player1 = "X";
-const currPlayer = player0;
-const gameOver = false;
-
-window.onload = function() {
-    setGame();
-}
-
-
+const restartBtn = document.getElementsByClassName('button')
+const box = document.getElementsByClassName('box')
+ console.log (box)
