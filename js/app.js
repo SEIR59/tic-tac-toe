99b9cde999a5e04339400fc555c3d5a01bd41839
Hoskyfull <norberto.cardenales@gmail.com>
2022-05-15 11:54:42 -0600

    html shel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('Hello frontend');
+const board;
+const player0 = "O";
+const player1 = "X";
+const currPlayer = player0;
+const gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+
