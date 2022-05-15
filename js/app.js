713d2fb0af84e38eb53bc8f3f9957c480b234014
mileslathan <mlathan.cn@gmail.com>
2022-05-15 17:36:12 -0400

    iter 3. Decided to create an array of winning conditions along with assign variables to represent html elements.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('Hello frontend');
+window.addEventListener("DomContentLoaded", () => {
+
+// Grabbing the elements needed from the HTML in variables.
+const squares = Array.from(document.querySelectorAll(".square"));
+const displayPlayer = document.querySelector(".turn-display")
+const gameResults = document.querySelector(".results")
+const restartButton = document.querySelector("#again");
+
+//Assigning an array to represent values for my board.
+let board = 
+["", "", "", 
+"", "", "",
+"", "", ""];
+
+//Assigning a variable to indicate the player's turn and if the game is on-going.
+let playerTurn = "X";
+let gameStatus = true;
+
+//Assigning end game if playerX or playerO won or was a tie
+const playerXWon = "Player X Won"
+const playerOWon = "Player O Won"
+
+    restartButton.addEventListener("click", boardReset)
+});
