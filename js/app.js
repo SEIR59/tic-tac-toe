0f0c7c4e8d8fe940a90e6db624aece32d8cb96e9
earlhalasan <earl.halasan@gmail.com>
2022-05-15 18:41:36 -0400

    Added once: true to my for each function to stop multiple clicks in one square

--- js/app.js
+++ js/app.js
@@ -5,26 +5,43 @@ console.log(squares);
 let squareArr = ["", "", "", "", "", "", "", "", ""];
 let firstPlayer = "X";
 let secondPlayer = "O";
-let turn;
 
 // length of turns
 // for (i = 0; i <= 9; i++) {}
 
-let playerTurn = [firstPlayer, secondPlayer];
-function currentTurn() {
-  for (i = 0; i < playerTurn.length; i += 1) {
-    return playerTurn[i];
-  }
-  return result;
-}
+// Tried to alternate between Xs and Os using a for loop
+// let playerTurn = [firstPlayer, secondPlayer];
+// function currentTurn() {
+//   for (i = 0; i < playerTurn.length; i++) {
+//     return playerTurn[i];
+//   }
+//   return result;
+// }
+// console.log(currentTurn);
 
 squares.forEach(function (square) {
-  square.addEventListener("click", function () {
-    square.innerText = firstPlayer;
-    firstPlayer = firstPlayer == "X" ? secondPlayer : "X";
-  });
+  square.addEventListener(
+    "click",
+    function () {
+      square.innerHTML = firstPlayer;
+      firstPlayer = firstPlayer == "X" ? secondPlayer : "X";
+    },
+    { once: true }
+  );
 });
 
+const winCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+
+// Repetitive Code
 // document.getElementById("box1").addEventListener("click", () => {
 //   //   let currentPlayer = [firstPlayer, secondPlayer];
 //   //   for (i = 0; i <= currentPlayer; i++) {
@@ -45,14 +62,3 @@ squares.forEach(function (square) {
 // document.addEventListener("click", () => {
 //   document.getElementById("box2").innerHTML = secondPlayer;
 // });
-
-const winCombo = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-];
