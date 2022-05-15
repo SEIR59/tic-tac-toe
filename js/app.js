f4981f9fb4282900eca335048a8d2f06091bb743
earlhalasan <earl.halasan@gmail.com>
2022-05-15 16:19:38 -0400

    Able to add XO to multiple squares but now need them to alternate between the two

--- js/app.js
+++ js/app.js
@@ -3,19 +3,40 @@ let gameBoard = document.querySelector(".square");
 let squareArr = ["", "", "", "", "", "", "", "", ""];
 let firstPlayer = "X";
 let secondPlayer = "O";
-let turn = 0;
-let lastClicked;
+// let turn = 0;
+// let lastClicked;
 
 // length of turns
-for (i = 0; i <= 9; i++) {}
+// for (i = 0; i <= 9; i++) {}
 
-document.addEventListener("click", () => {
-  document.getElementById("box1").innerHTML = firstPlayer;
+document.getElementById("box1").addEventListener("click", () => {
+  //   let currentPlayer = [firstPlayer, secondPlayer];
+  //   for (i = 0; i <= currentPlayer; i++) {
+  //     console.log(currentPlayer[i]);
+  //   }
+  document.getElementById("box1").innerHTML = "X";
 });
 
-document.addEventListener("click", () => {
-  document.getElementById("box2").innerHTML = secondPlayer;
+document.getElementById("box2").addEventListener("click", () => {
+  //   let currentPlayer = [firstPlayer, secondPlayer];
+  //   for (i = 0; i <= currentPlayer; i++) {
+  //     console.log(currentPlayer[i]);
+  //   }
+  document.getElementById("box2").innerHTML = "O";
 });
+
+document.getElementById("box3").addEventListener("click", () => {
+  //   let currentPlayer = [firstPlayer, secondPlayer];
+  //   for (i = 0; i <= currentPlayer; i++) {
+  //     console.log(currentPlayer[i]);
+  //   }
+  document.getElementById("box3").innerHTML = "X";
+});
+
+// Trying to alternate X and O with one event listener
+// document.addEventListener("click", () => {
+//   document.getElementById("box2").innerHTML = secondPlayer;
+// });
 
 const winCombo = [
   [0, 1, 2],
