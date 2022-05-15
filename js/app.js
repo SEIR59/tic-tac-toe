e461bcbfe61adc0477aee92de97987b8ccce72f8
mileslathan <mlathan.cn@gmail.com>
2022-05-15 17:48:31 -0400

    iter 4 I'm a visual person so I decided to create an variable with arrays that represent the win conditions.

--- js/app.js
+++ js/app.js
@@ -19,6 +19,19 @@ let gameStatus = true;
 //Assigning end game if playerX or playerO won or was a tie
 const playerXWon = "Player X Won"
 const playerOWon = "Player O Won"
+const endTie = "Its a Tie!"
 
+// Creating a variable that will be used as a comparison to check if a player has reached any of the following win conditions represented by the borad itself.
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
     restartButton.addEventListener("click", boardReset)
 });
