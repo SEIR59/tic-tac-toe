1609de5a2bad93abc4f9ea1d96ff9d885272d786
annakoanna <anna.231991k@gmail.com>
2022-05-16 00:08:59 -0700

    add function to JS

--- js/app.js
+++ js/app.js
@@ -5,3 +5,8 @@ const contentWrapper = document.getElementById("content");
 const modalResult = document.getElementById("result-wrapper");
 const restart = document.getElementById("restart");
 
+function changeContent(content) {
+    contentWrapper.innerHTML = content;
+    modalResult.style.display = 'block';
+}
+
