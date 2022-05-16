4b59b9f81a210444f158b4170a110fd9f103b9d0
mileslathan <mlathan.cn@gmail.com>
2022-05-15 21:29:31 -0400

    iter 6. Added an event listener for each square and began to write function for conditional logic

--- js/app.js
+++ js/app.js
@@ -4,8 +4,10 @@ const player2 = "O";
 let currentPlayer = player1;
 
 //Creating Variables for each of the elements for insertion.
-const squares = document.querySelectorAll(".square")
-const gameBoard = document.querySelector(".board")
+const squares = document.querySelectorAll(".square");
+const endGameBox = document.getElementById("endGameArea")
+const endResults = document.getElementById("results");
+const gameReset = document.getElementById("again");
 
 // Creating the state of the game in an array and we are nulling the board for resets.
 const gameState = Array(squares.length);
@@ -16,3 +18,13 @@ if (gameBoard === null) {
     currentPlayer = player1
 }
 console.log(gameState);
+
+// A quick loop that adds an Event Listener to each of our squares to then run the function squareClick.
+squares.forEach(square=>square.addEventListener("click", squareClick));
+
+// A function that checks if the endGameArea from our html is visible, stops the game from going further with return;
+function squareClick(event) {
+    if (endGameBox.classList.contains("visible")){
+        return;
+    }
+}
