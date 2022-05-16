bf779a99b27f7b95ec5cc122eac7281f06764863
annakoanna <anna.231991k@gmail.com>
2022-05-16 00:11:34 -0700

    ADD eventlistner and function

--- js/app.js
+++ js/app.js
@@ -10,3 +10,18 @@ function changeContent(content) {
     modalResult.style.display = 'block';
 }
 
+board.addEventListener('click', e => {
+    if(e.target.className = "tile") {
+        let nextPlayer = move % 2 === 0 ? "0" : "X";
+        let currentPlayer = move % 2 === 0 ? "X" : "0";
+        e.target.innerHTML = currentPlayer;
+        move++;
+        changeContent(`player ${nextPlayer}'s turn`);
+        check();
+        if (move === 9 && result === "") {
+            changeContent("Tie");
+        }
+        
+    }
+})
+
