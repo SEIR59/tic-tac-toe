5dead798f9249763d8817d6e8290c2ff9657af60
patrickbasham <PatrickTBasham@gmail.com>
2022-05-15 21:00:36 -0400

    Fixed win conditions, made scores track

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-console.log('Hello frontend');
+const gameboard = document.getElementById('gameboard');
+const gameboard_squares = document.getElementsByClassName('gameboard-square');
+const turn_status = document.getElementById('turn-status');
+const reset_btn = document.getElementById('reset-btn');
+const player1Score = document.getElementById('player1Score');
+const player2Score = document.getElementById('player2Score');
 
-// 1. add an event listener to each div with .squares class using a for loop, treating it as an array.
-    // a. will need to add a reset that will re add event listeners after game is complete or reset is clicked.
-    
-    // 3. set up variables for both player and ai symbol, 
-    //     a. set up score tracker for each,
-// make this random?
 
+let vsAi = true;
 const playerSymbol = 'X';
 const aiSymbol = 'O';
 let turnCounter = 0;
@@ -25,58 +25,55 @@ const square_7 = document.getElementById('square7');
 const square_8 = document.getElementById('square8');
 const square_9 = document.getElementById('square9');
 
-
-function checkWinCondition() {
-    // check if there are three of the same symbol in a row anywhere
-    console.log("checking win conditions")
-    if (square_1.innerHTML === currentSymbol && square_2.innerHTML === currentSymbol && square_3.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_5.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_6.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_7.innerHTML === currentSymbol && square_8.innerHTML === currentSymbol && square_9.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_1.innerHTML === currentSymbol && square_4.innerHTML === currentSymbol && square_7.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_2.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_8.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_3.innerHTML === currentSymbol && square_6.innerHTML === currentSymbol && square_9.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_1.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_9.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } else if (square_3.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_7.innerHTML === currentSymbol) {
-        alert('Player Won!');
-    } 
-}
-
-// funciton that removes all event listeners, dsplayes winner and updates score
-function someoneWon() {
-    //update score of player that one, remove remaining event listeners.
-    if (turnCounter % 2 === 0) {
-        playerWins++;
-    } else {
-        aiWins++;
+// add event listeners
+function setEventListeners() {
+    for (let i = 0; i < gameboard_squares.length; i++) {
+        gameboard_squares[i].addEventListener('click', gameboard_click);
+        gameboard_squares[i].innerHTML = '';
+        if (!gameboard_squares[i].classList.contains('square-hover')) {
+            gameboard_squares[i].classList.add('square-hover');
+        }
     }
-    currentSymbol = 'X';
+    if (gameboard.classList.contains('winnerwinner')) {
+        gameboard.classList.remove('winnerwinner');
+    }
 }
-function gameboard_reset() {
-    // reset game but keep scores
-    turnCounter = 0;
- }
 
-const gameboard_squares = document.getElementsByClassName('gameboard-square');
-const turn_status = document.getElementById('turn-status');
+setEventListeners();
 
+reset_btn.addEventListener('click', gameboard_reset);
+
+
+////*     functions     *////
+
+// set / reset gameboard
+  // remove playerx class
+  // add event listeners back
+  // clear board
+  // reset counters -- creawte function for setting these elements
+
+// on square click
 function gameboard_click(e) {
-    // changes the classes of the div that is clicked,
-    // removes the event listener 
-    // and sets up for the next players turn.
-    console.log('calling gameboard_click')
     currentSquare = e.target;
     currentSquare.innerHTML = currentSymbol;
     currentSquare.classList.toggle('square-hover')
+    if (turnCounter % 2 === 0) {
+        currentSquare.classList.add('player1-choice');
+    } else {
+        currentSquare.classList.add('player2-choice');
+    }
+    console.log(gameboard_squares.classList);
     currentSquare.removeEventListener('click', gameboard_click);
     turnCounter++;
-    turn_status.innerText = `It's player ${turnCounter % 2 + 1}'s turn!`
+    if (vsAi === true) {
+        if (turnCounter % 2 === 0) {
+            turn_status.innerText = `It's player ${turnCounter % 2 + 1}'s turn!`
+        } else {
+            turn_status.innerText =  'It\'\s the Ai\'\s turn!'
+        }
+    } else {
+        turn_status.innerText = `It's player ${turnCounter % 2 + 1}'s turn!`
+    }
     console.log(turnCounter)
     
     // check win condition
@@ -90,12 +87,104 @@ function gameboard_click(e) {
 
     // if you did not win, allow the compute to check and then check win condition again.
     // computer move -- random for now
+    if (vsAi === true) {
+        // ai chooses randomly
+        // make ai randomly pick 1 - 9 until it get's an empty space
+        // while (true) {
+        //     let aiChoice = Math.floor(Math.random() * 9);
+        //     if (gameboard_squares[aiChoice] === "") {
+        //         // wait a few seconds, randomize the length between 2 - 4 seconds?
 
-    // check win condition
+        //         // change square
+                
+        //     }
+        // }
+    }
+}
+
+
+function gameboard_reset() {
+    // reset game but keep scores
+    vsAi = true;
+    turnCounter = 0;
+    currentSymbol = 'X';
+
+    // if it has class of playervalue remove it
+    
+    // add all toggle class if not have
+
+
+
+    setEventListeners()
+ }
+
+ function checkWinCondition() {
+    // check if there are three of the same symbol in a row anywhere
+    console.log("checking win conditions")
+    let winner = false;
+    if (square_1.innerHTML === currentSymbol && square_2.innerHTML === currentSymbol && square_3.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_4.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_6.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_7.innerHTML === currentSymbol && square_8.innerHTML === currentSymbol && square_9.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_1.innerHTML === currentSymbol && square_4.innerHTML === currentSymbol && square_7.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_2.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_8.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_3.innerHTML === currentSymbol && square_6.innerHTML === currentSymbol && square_9.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_1.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_9.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    } else if (square_3.innerHTML === currentSymbol && square_5.innerHTML === currentSymbol && square_7.innerHTML === currentSymbol) {
+        winner = true;
+        someoneWon(winner)
+    }
+    if (turnCounter === 9 && winner === false) {
+        console.log('its a tie!');
+        someoneWon(winner)
+    }
+}
+
+function someoneWon(tie) {
+    // flip board
+    gameboard.classList.add('winnerwinner');
+
+
+    //update score of player that one, remove remaining event listeners.
+    if (!tie) {
+        // change text to tie!
+        turn_status.innerHTML = 'It\'\s a tie!';
+    } else if (turnCounter % 2 === 0) {
+        turn_status.innerHTML = 'The Ai wins!';
+        aiWins++;
+        player2Score.innerHTML =  aiWins;
+    } else {
+        turn_status.innerHTML = 'Player 1 wins!';
+        playerWins++;
+        player1Score.innerHTML = playerWins;
+    }
+    
+    // remove event listeners and change text to winner or tie.
+    for (let i = 0; i < gameboard_squares.length; i++) {
+        gameboard_squares[i].removeEventListener('click', gameboard_click);
+        if (gameboard_squares[i].classList.contains('square-hover')) {
+            gameboard_squares[i].classList.remove('square-hover');
+        }
+    }
 
 
 }
 
-for (let i = 0; i < gameboard_squares.length; i++) {
-    gameboard_squares[i].addEventListener('click', gameboard_click);
+
+function playAgain() {
+
 }
+/****     end functions     ****/
