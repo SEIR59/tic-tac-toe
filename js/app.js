513b81985aca33e713a34bd3be8b9969fe72594d
andersons1218 <andersons1218@gmail.com>
2022-05-15 23:47:20 -0400

    tic-tac-toe update

--- js/app.js
+++ js/app.js
@@ -2,16 +2,31 @@
 const boxes= document.querySelectorAll(".box");
 const board = document.querySelector('.board');
 const button = document.querySelector('.button');
+const winCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+
+]
+
+// this code lets the squares change from x to o
 
 let user = 'x'
 for (const box of boxes){
     box.addEventListener("click", function(){
-        box.innerHTML = user;
-        if ( user === x );
-        else( user = o);
-        box.innerHTML = o
-    }) 
+        if(user === "X"){
+            box.innerHTML = "X";
+            user = "O";
+        } else {
+           box.innerHTML = "O";
+           user = "X";
+        }
     
+    })
 }
-
 
