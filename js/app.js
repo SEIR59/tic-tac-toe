36879fe0ec367b5549d67cf620ca5cd55c86d3b5
annakoanna <anna.231991k@gmail.com>
2022-05-16 00:06:30 -0700

    add DOM to javascript

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const board = document.getElementById("board");
+let move = 0;
+let result = "";
+const contentWrapper = document.getElementById("content");
+const modalResult = document.getElementById("result-wrapper");
+const restart = document.getElementById("restart");
+
