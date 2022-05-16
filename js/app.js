090b00c4b4b8cea84fe6d53361c1c06778e668ad
idbic <dbicga@gmail.com>
2022-05-16 06:47:25 -0500

    Final Commit TicTacToe

--- js/app.js
+++ js/app.js
@@ -10,11 +10,7 @@ let idEighth = document.querySelector("#eighth");
 let idNinth = document.querySelector("#ninth");
 let divGrid = document.querySelector(".divGrid")
 let p = document.getElementById('p')
-const board = [idFirst, idSecond, idThird, idFourth, idFifth, idSixth, idSeventh, idEighth, idNinth]
 
-let playerX = null
-let playerO = null
-let moveTracker = null
 
 let inputX = "❌";
 
@@ -34,42 +30,239 @@ let inputX = "❌";
 // create array win conditional or tie etc
 
 
+function winCheck(){
+  let id1 = document.getElementById('first').innerText
+  let id2 = document.getElementById('second').innerText
+  let id3 = document.getElementById('third').innerText
+  let id4 = document.getElementById('fourth').innerText
+  let id5 = document.getElementById('fifth').innerText
+  let id6 = document.getElementById('sixth').innerText
+  let id7 = document.getElementById('seventh').innerText
+  let id8 = document.getElementById('eighth').innerText
+  let id9 = document.getElementById('ninth').innerText
+  
+  if((id1 ===  '❌') && (id2 === '❌') && (id3 === '❌')) {
+  p.innerHTML = "Player ❌ won";
+ idFourth = idFourth.disabled
+ idFifth = idFifth.disabled
+ idSixth = idSixth.disabled
+ idSeventh = idSeventh.disabled
+ idEighth = idEighth.disabled
+ idNinth = idNinth.disabled
+  
+ } else if((id4 ===  '❌') && (id5 === '❌') && (id6 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled 
+  idThird = idThird.disabled
+  idSeventh = idSeventh.disabled 
+  idEighth = idEighth.disabled 
+  idNinth = idNinth.disabled 
+  
+} else if((id7 ===  '❌') && (id8 === '❌') && (id9 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled 
+  idThird = idThird.disabled
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  
+} else if((id1 ===  '❌') && (id4 === '❌') && (id7 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  
+  idSecond = idSecond.disabled 
+  idThird = idThird.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  idEighth = idEighth.disabled 
+  idNinth = idNinth.disabled
+  
+} else if((id2 ===  '❌') && (id5 === '❌') && (id8 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  idThird = idThird.disabled
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled 
+  idNinth = idNinth.disabled
+  
+} else if((id3 ===  '❌') && (id6 === '❌') && (id9 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSeventh = idSeventh.disabled
+  idEighth = idEighth.disabled 
+  
+  
+} else if((id1 ===  '❌') && (id5 === '❌') && (id9 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  idSecond = idSecond.disabled
+  idThird = idThird.disabled
+  idFourth = idFourth.disabled
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled
+  idEighth = idEighth.disabled 
+  
+  
+} else if((id3 ===  '❌') && (id5 === '❌') && (id7 === '❌')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ❌ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled
+  idFourth = idFourth.disabled
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled
+  idEighth = idEighth.disabled
+  idNinth = idNinth.disabled 
+  
+  
+} else if((id1 ===  '⭕') && (id2 === '⭕') && (id3 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled 
+  idEighth = idEighth.disabled 
+  idNinth = idNinth.disabled 
+  
+} else if((id4 ===  '⭕') && (id5 === '⭕') && (id6 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled 
+  idThird = idThird.disabled
+  idSeventh = idSeventh.disabled 
+  idEighth = idEighth.disabled 
+  idNinth = idNinth.disabled 
+  
+} else if((id7 ===  '⭕') && (id8 === '⭕') && (id9 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled 
+  idThird = idThird.disabled
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  
+} else if((id1 ===  '⭕') && (id4 === '⭕') && (id7 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  
+  idSecond = idSecond.disabled 
+  idThird = idThird.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  idEighth = idEighth.disabled 
+  idNinth = idNinth.disabled
+  window.alert('Player X won');
+} else if((id2 ===  '⭕') && (id5 === '⭕') && (id8 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idThird = idThird.disabled
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled 
+  idNinth = idNinth.disabled
+  
+} else if((id3 ===  '⭕') && (id6 === '⭕') && (id9 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled
+  idFourth = idFourth.disabled
+  idFifth = idFifth.disabled 
+  idSeventh = idSeventh.disabled
+  idEighth = idEighth.disabled 
+  
+  
+} else if((id1 ===  '⭕') && (id5 === '⭕') && (id9 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idSecond = idSecond.disabled
+  idThird = idThird.disabled
+  idFourth = idFourth.disabled
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled
+  idEighth = idEighth.disabled 
+  
+  window.alert('Player X won');
+} else if((id3 ===  '⭕') && (id5 === '⭕') && (id7 === '⭕')) {
+  console.log("winnerwinner")
+  p.innerHTML = "Player ⭕ won";
+  idFirst = idFirst.disabled
+  idSecond = idSecond.disabled
+  idFourth = idFourth.disabled
+  idSixth = idSixth.disabled
+  idSeventh = idSeventh.disabled
+  idEighth = idEighth.disabled
+  idNinth = idNinth.disabled 
+  
+  
+} else if ((id1 === '❌' || id1 === '⭕') && (id2 === '❌'
+|| id2 === '⭕') && (id3 === '❌' || id3 === '⭕') &&
+(id4 === '❌' || id4 === '⭕') && (id5 === '❌' ||
+id5 === '⭕') && (id6 === '❌' || id6 === '⭕') &&
+(id7 === '❌' || id7 === '⭕') && (id8 === '❌' ||
+id8 === '⭕') && (id9 === '❌' || id9 === '⭕')) {
+    p.innerHTML = "Tie game!";
+    
+}
+
+}
+
+
 // Created a function to /see if an x or o was in the box the function then disables that cell
 
 function markBox1() {
   if(inputX === "❌") {
     idFirst.innerText = "❌";
-    p.innerText = "⭕'s turn"
-    idFirst = idFirst.disabled
-    inputX = "⭕"
+    p.innerText = "⭕'s turn";
+    inputX = "⭕";
+    idFirst.disabled = true;
     
   } else if(inputX === "⭕"){
     idFirst.innerText = "⭕";
-    p.innerText = "❌'s turn"
-    idFirst = idFirst.disabled 
-    inputX = "❌"
+    p.innerHTML = "❌'s turn";
+    inputX = "❌";
+    idFirst.disabled = true;
    } 
 }
 
+
 idFirst.addEventListener('click', markBox1)
+idFirst.addEventListener('click', winCheck)
 
 
 function markBox2() {
   if(inputX === "❌") {
-    idSecond.innerText = "❌";
     p.innerText = "⭕'s turn"
-    idSecond = idSecond.disabled
+    idSecond.innerText = "❌";
+    idSecond.disabled = true
     inputX = "⭕";
     
   } else {
     idSecond.innerText = "⭕";
     p.innerText = "❌'s turn"
     inputX = "❌";
-    idSecond = idSecond.disabled 
+    idSecond.disabled = true
   }
 }
 
 idSecond.addEventListener('click', markBox2)
+idSecond.addEventListener('click', winCheck)
+
 
 
 
@@ -78,16 +271,17 @@ function markBox3() {
     idThird.innerText = "❌";
     p.innerText = "⭕'s turn"
     inputX = "⭕";
-    idThird = idThird.disabled
-  } else {
+    idThird.disabled = true
+  } else if(inputX === "⭕") {
     idThird.innerText = "⭕";
     p.innerText = "❌'s turn"
     inputX = "❌";
-    idThird = idThird.disabled
+    idThird.disabled = true
   }
 }
 
 idThird.addEventListener('click', markBox3)
+idThird.addEventListener('click', winCheck)
 
 
 
@@ -96,16 +290,17 @@ function markBox4() {
     idFourth.innerText = "❌"
     p.innerText = "⭕'s turn"
     inputX = "⭕"
-    idFourth = idFourth.disabled
-  } else {
+    idFourth.disabled = true
+  } else if(inputX === "⭕"){
     idFourth.innerText = "⭕"
     p.innerText = "❌'s turn"
     inputX = "❌"
-    idFourth = idFourth.disabled
-  }
+    idFourth.disabled = true
+  } 
 }
 
 idFourth.addEventListener('click', markBox4)
+idFourth.addEventListener('click', winCheck)
 
 
 
@@ -114,16 +309,18 @@ function markBox5() {
     idFifth.innerText = "❌"
     p.innerText = "⭕'s turn"
     inputX = "⭕"
-    idFifth = idFifth.disabled
+    idFifth.disabled = true 
+  
   } else {
     idFifth.innerText = "⭕"
     p.innerText = "❌'s turn"
     inputX = "❌"
-    idFifth = idFifth.disabled
+    idFifth.disabled = true
   }
 }
 
 idFifth.addEventListener('click', markBox5)
+idFifth.addEventListener('click', winCheck)
 
 
 
@@ -132,17 +329,18 @@ function markBox6() {
     idSixth.innerText = "❌"
     p.innerText = "⭕'s turn"
     inputX = "⭕"
-    idSixth = idSixth.disabled
+    idSixth.disabled = true
 
   } else {
     idSixth.innerText = "⭕"
     p.innerText = "❌'s turn"
     inputX = "❌"
-    idSixth = idSixth.disabled
+    idSixth.disabled = true
   }
 }
 
 idSixth.addEventListener('click', markBox6)
+idSixth.addEventListener('click', winCheck)
 
 
 
@@ -151,17 +349,17 @@ function markBox7() {
     idSeventh.innerText = "❌"
     p.innerText = "⭕'s turn"
     inputX = "⭕"
-    idSeventh = idSeventh.disabled
+    idSeventh.disabled = true
   } else {
     idSeventh.innerText = "⭕"
     p.innerText = "❌'s turn"
     inputX = "❌"
-    idSeventh = idSeventh.disabled
+    idSeventh.disabled = true
   }
 }
 
 idSeventh.addEventListener('click', markBox7)
-
+idSeventh.addEventListener('click', winCheck)
 
 
 function markBox8() {
@@ -169,16 +367,17 @@ function markBox8() {
     idEighth.innerText = "❌"
     p.innerText = "⭕'s turn"
     inputX = "⭕"
-    idEighth = idEighth.disabled
+    idEighth.disabled = true
   } else {
     idEighth.innerText = "⭕"
     p.innerText = "❌'s turn"
     inputX = "❌"
-    idEighth = idEighth.disabled
+    idEighth.disabled = true
   }
 }
 
 idEighth.addEventListener('click', markBox8)
+idEighth.addEventListener('click', winCheck)
 
 
 
@@ -187,16 +386,17 @@ function markBox9() {
     idNinth.innerText = "❌"
     p.innerText = "⭕'s turn"
     inputX = "⭕"
-    idNinth = idNinth.disabled
+    idNinth.disabled = true
   } else {
     idNinth.innerText = "⭕"
     p.innerText = "❌'s turn"
     inputX = "❌"
-    idNinth = idNinth.disabled
+    idNinth.disabled = true 
   }
 }
 
 idNinth.addEventListener('click', markBox9)
+idNinth.addEventListener('click', winCheck)
 
 
 
@@ -224,4 +424,5 @@ document.querySelector('#reset-button').addEventListener('click', reset)
 
 console.log('wtf happened')
 
-//working on solution for checking the answers now
+//checking tie
+
