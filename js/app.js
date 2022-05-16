4fc87b6911b386b231a5e250e0ffd1a2b20ed262
jrbreeden <jrbreeden55@gmail.com>
2022-05-15 21:31:02 -0400

    played the game

--- js/app.js
+++ js/app.js
@@ -4,19 +4,23 @@ const O = "O";
 const X = "X";
 
 let currentPlayer = O;
-
-
+const gameboard = new Array(9).fill(null);
 
 const squareClicked = (e) => {
     const id = e.target.id;
   
-e.target.innerText = currentPlayer;  
-   
-  currentPlayer = 
-    (currentPlayer === O) ? X : O;
-  };
+if (!gameboard[id]){
+  gameboard[id] = currentPlayer;
+  e.target.innerText = currentPlayer; 
+  
+  currentPlayer = currentPlayer === O ? X : O
+}
+  
+  console.log(gameboard)
+};
 
 squares.forEach((square) => square.addEventListener("click", squareClicked));
+
 
 
   
