0b15b9305a0d4506df3bc02d939d8c3180813107
mkhan42 <michallekhan9@gmail.com>
2022-05-16 00:11:15 -0400

    Reset button does not work

--- js/app.js
+++ js/app.js
@@ -8,12 +8,10 @@ let currentTurn = 'player1';
 
 
 const gridClass = document.querySelectorAll('.grid-class');
-const test = document.querySelectorAll('.test');
-// const test2 = document.querySelector('.test');
 const btn = document.querySelector('.btn');
 
 const currPlay = document.querySelector('#player-turn');
-console.log(currPlay);
+
 
 let checkForWinner = (playerArray) => {
     let isWinner = false
@@ -31,44 +29,63 @@ let checkForWinner = (playerArray) => {
 
 let moves = 0;
 
-for(let i = 0; i < 9; i++){
-    gridClass[i].addEventListener('click', (event) => {
-        if(currentTurn === 'player1'){
-            gridClass[i].innerHTML = "X"
-            playerOneVal.push(i);
-            console.log(`Player one val is: ${playerOneVal}`);
-            test[i].style.pointerEvents = 'none';
-            moves++;
-            currentTurn = 'player2';
-            currPlay.innerHTML = "Player Two Turn"
-            if(moves === 9) {
-                currPlay.innerHTML = "Cat's Game"
-            }
-        } else if(currentTurn === 'player2'){
-            gridClass[i].innerHTML = "O";
-            playerTwoVal.push(i);
-            console.log(`Player two val is: ${playerTwoVal}`);
-            test[i].style.pointerEvents = 'none';
-            moves++;
-            currentTurn = 'player1'
-            currPlay.innerHTML = "Player One Turn"
-            if(moves === 9) {
-                currPlay.innerHTML = "Cat's Game"
-            }
-        }
-        const isPlayerOneWinner = checkForWinner(playerOneVal);
-        const isPlayerTwoWinner = checkForWinner(playerTwoVal);
-        if(isPlayerOneWinner){
-            currPlay.innerHTML = "Player One Wins"
-        } 
-        else if(isPlayerTwoWinner){
-            currPlay.innerHTML = "Player Two Wins"
-        }
-        
-        btn.addEventListener("click", (event) => {
-            gridClass[i].innerHTML = " ";
-        });
+function playGame() {
 
-    })
-    
+    for(let i = 0; i < 9; i++){
+        gridClass[i].addEventListener('click', (event) => {
+            if(currentTurn === 'player1'){
+                gridClass[i].innerHTML = "X"
+                playerOneVal.push(i);
+                gridClass[i].style.pointerEvents = 'none';
+                moves++;
+                currentTurn = 'player2';
+                currPlay.innerHTML = "Player Two's Turn"
+                if(moves === 9) {
+                    currPlay.innerHTML = "Cat's Game"
+                }
+            } else if(currentTurn === 'player2'){
+                gridClass[i].innerHTML = "O";
+                playerTwoVal.push(i);
+                gridClass[i].style.pointerEvents = 'none';
+                moves++;
+                currentTurn = 'player1'
+                currPlay.innerHTML = "Player One's Turn"
+                if(moves === 9) {
+                    currPlay.innerHTML = "Cat's Game"
+                }
+            }
+            const isPlayerOneWinner = checkForWinner(playerOneVal);
+            const isPlayerTwoWinner = checkForWinner(playerTwoVal);
+            if(isPlayerOneWinner){
+                currPlay.innerHTML = "Player One Wins"
+            } 
+            else if(isPlayerTwoWinner){
+                currPlay.innerHTML = "Player Two Wins"
+            }
+        })  
+    }
+
 }
+
+
+// let clicked = false;
+
+// function reset() {
+//     playerOneVal.splice(0, playerOneVal.length);
+//     playerTwoVal.splice(0, playerTwoVal.length);
+//     currentTurn = 'player1';
+//     for(let i = 0; i < 9; i++){
+//         btn.addEventListener("click", (event) => {
+//             clicked = true;
+//             gridClass[i].innerHTML = " ";
+//             test[i].style.pointerEvents = 'auto';
+//             playGame();
+//             currPlay.innerHTML = "Player One Turn";
+//         });
+//     }
+// }
+
+playGame();
+// reset();
+
+
