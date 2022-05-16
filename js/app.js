be04b1a024860aa98cd33239726a465fb7ee82e1
Jason Lull <jlull7774@gmail.com>
2022-05-16 09:21:27 -0400

    comments added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// here i am declaring some things we already know and making connections from html to js 
+
 const boxes = document.querySelectorAll(".box");
 const playerX = "X";
 const playerO = "O";
@@ -11,10 +13,15 @@ const interface = document.getElementById("interface");
 const interfaceText = document.getElementById("interface-text")
 const resetButton = document.getElementById("reset")
 
+// this is making the button able to reset which i declare later on.
 resetButton.addEventListener("click", resetGame);
 
+
+// this is making the function boxClick able to use on ALL boxes in tic-tac-toe
 boxes.forEach((box) => box.addEventListener("click", boxClick));
 
+
+// this is the function which is used for the box click. if it is empty then player x - x, now it is playerO turn. and vise versa. at the end it will run the function check for win. if no win then exit function and display checkForWin function
 function boxClick(event) {
     if (interface.classList.contains('show')) {
         return;
@@ -39,7 +46,7 @@ function boxClick(event) {
 
     checkForWin();
 }
-
+// this function check all avaliable outcomes of the game and displays gameWon box of text that tells the user who won and allows them to reset the game. 
 function checkForWin() {
     for (const winCondtion of winCondtions) {
         const { lineOf, strikeClass } = winCondtion;
@@ -61,7 +68,7 @@ function checkForWin() {
         gameWon(null);
     }
 }
-
+// this references the (interface) in html and reset game button
 function gameWon(winnerText) {
     let text = "Cat!"
     if (winnerText != null) {
@@ -70,7 +77,7 @@ function gameWon(winnerText) {
     interface.className = "show";
     interfaceText.innerText = text
 }
-
+//  these are all the possible outcomes in tic-tac-toe minus cat
 const winCondtions = [
     { lineOf: [1, 2, 3], strikeClass: "row-1" },
     { lineOf: [4, 5, 6], strikeClass: "row-2" },
@@ -83,7 +90,7 @@ const winCondtions = [
     { lineOf: [3, 6, 9], strikeClass: "diagonal-3" },
 
 ];
-
+// this is what the reset button will do. erase the strike element, hide the interface, unshow all the x's and o's. and set the player turn to playerX
 function resetGame () {
     strike.className = "strike";
     interface.className = "hide";
