dd2e5bee66418e6924955fe42d80ff940878d69e
earlhalasan <earl.halasan@gmail.com>
2022-05-16 00:30:41 -0400

    Game functions now. Debugged for a while in the gameRules function because each click was logging as the opposite inputs. Xs were Os and Os were Xs when placed into the arrary. Realized it was the order of choicesI equals firstPlayer and had to place over the alternating function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,12 @@
 let squares = document.querySelectorAll(".square");
 let message = document.querySelector(".message");
 let button = document.querySelector("button");
-let choices = [];
+let choices = ["", "", "", "", "", "", "", "", ""];
+
 squares = Array.from(squares);
-console.log(squares);
 
 let firstPlayer = "X";
 let secondPlayer = "O";
-
-function gameRules() {}
-squares.forEach(function (square) {
-  square.addEventListener(
-    "click",
-    function () {
-      square.innerHTML = firstPlayer;
-      firstPlayer = firstPlayer == "X" ? secondPlayer : "X";
-      message.innerHTML = `${firstPlayer}'s turn!`;
-    },
-    { once: true }
-  );
-});
 
 const winCombo = [
   [0, 1, 2],
@@ -32,23 +19,33 @@ const winCombo = [
   [2, 5, 8],
 ];
 
-for (i = 0; i < winCombo.length; i++) {
-  let winner = winCombo[i];
-  let a = squares[winner[0]];
-  let b = squares[winner[1]];
-  let c = squares[winner[2]];
+function gameRules(square, i) {
+  if (square.innerHTML.trim() != "") return;
+  square.innerHTML = firstPlayer;
+  console.log(firstPlayer);
+  choices[i] = firstPlayer;
+  firstPlayer = firstPlayer == "X" ? secondPlayer : "X";
+  console.log(choices);
+  message.innerHTML = `${firstPlayer}'s turn!`;
 
-  if (a == "" || b == "" || c == "") {
-    console.log("Player won!");
-  }
-  if (a == b && b == c) {
-    message.innerHTML = `Player ${a} won!`;
+  for (i = 0; i < winCombo.length; i++) {
+    let winner = winCombo[i];
+    let a = choices[winner[0]];
+    let b = choices[winner[1]];
+    let c = choices[winner[2]];
+
+    if (a == "" || b == "" || c == "") {
+      continue;
+    }
+    if (a == b && b == c) {
+      message.innerHTML = `Player ${a} won! Confetti is falling from the sky. Champagne is flowing. People are asking you to kiss their newborns on the forehead!`;
+    }
   }
 }
-
-// squares.forEach((square, i) => {
-//   square.addEventListener("click", () => ticTacToe(square, i));
-// });
+// THIS GOES THROUGH EACH SQUARE AND ADDS AN EVENTLISTENER AND GIVES AN INDEX
+squares.forEach((square, i) => {
+  square.addEventListener("click", () => gameRules(square, i));
+});
 
 // button.addEventListener("click", () => {
 //   squares.innerHTML = "X";
