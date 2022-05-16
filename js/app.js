06c3b2c0b4c1e3ca7464de0f0aebf7242c75c325
patrickbasham <PatrickTBasham@gmail.com>
2022-05-15 23:40:11 -0400

    Added comments and fixed misc text

--- js/app.js
+++ js/app.js
@@ -27,7 +27,9 @@ const square_7 = document.getElementById('square7');
 const square_8 = document.getElementById('square8');
 const square_9 = document.getElementById('square9');
 
-// add event listeners
+////*     functions     *////
+// set the boards classes / event listeners to their default.
+// ALSO removes / sets a class to allow for an animation every time a point is added to someone.
 function setBoard() {
     for (let i = 0; i < gameboard_squares.length; i++) {
         gameboard_squares[i].addEventListener('click', gameboard_click);
@@ -57,10 +59,9 @@ playAgain_btn.addEventListener('click', gameboard_reset);
 reset_btn.addEventListener('click', gameboard_complete_reset);
 
 
-////*     functions     *////
 
 
-// on square click
+// when a square is clicked, adjust classes approprietly and then tell ai to choose.
 function gameboard_click(e) {
     currentSquare = e.target;
     currentSquare.innerHTML = currentSymbol;
@@ -74,6 +75,7 @@ function gameboard_click(e) {
 
     console.log(turnCounter)
     
+    // removing listerners / classes to stop player input on ai turn / win/ lose screen.
     for (let i = 0; i < gameboard_squares.length; i++) {
         gameboard_squares[i].removeEventListener('click', gameboard_click);
         if (gameboard_squares[i].classList.contains('square-hover')) {
@@ -83,26 +85,30 @@ function gameboard_click(e) {
     // check win condition
     let gameOver = checkWinCondition();
 
-
+    // if player has not won, allow ai to go.
     if (gameOver === false) {
         aiTurn();
     }
 
 }
 
+// the ai's turn.
 function aiTurn() {
-        
+    
+    // get random interval between 1 and 4 seconds for the ai's response wait time.
     let waitRandom = (Math.floor(Math.random() * 4) + 1) * 1000;
     let spotCheck = 0;
     console.log('Ai is thinking...');
     setTimeout(function(){
 
         while (true) {
+            // this loop allows the ai to make a random selection between 1 - 9 and only changes the square it chooses once it finds an empty one. I'd like to save this so i can go back in the future and add the mini-max algorithm once i learn more aobut it.
             aiChoice = Math.floor(Math.random() * 9)
             console.log(aiChoice);
             console.log(gameboard_squares[aiChoice]);
             if (gameboard_squares[aiChoice].innerHTML === '') {
                 console.log('we made it into here');
+                // adjust classes and event listners for the ai's choice.
                 gameboard_squares[aiChoice].innerHTML = currentSymbol;
                 gameboard_squares[aiChoice].classList.remove('square-hover')
                 gameboard_squares[aiChoice].classList.add('player2-choice');
@@ -113,11 +119,13 @@ function aiTurn() {
                 break;
             }
             spotCheck++;
+            // if the ai isn't able to find a spot to place then it breaks out of the infinite loop.
             if (spotCheck > 100) {
                 console.log('break check-loop...')
                 break;
             }
         }
+        // check if the ai has won and if not allow the player to make a selection again. (Gives classes / event listeners to empty squared.)
         if (checkWinCondition() === false) {
             for (let i = 0; i < gameboard_squares.length; i++) {
                 if (gameboard_squares[i].innerHTML === '') {
@@ -136,6 +144,7 @@ function aiTurn() {
     },waitRandom)
 }
 
+// resets gameboard and scored
 function gameboard_complete_reset() {
     vsAi = true;
     turnCounter = 0;
@@ -147,6 +156,7 @@ function gameboard_complete_reset() {
     spotCheck = 0;
     setBoard()
 }
+// only resets gameboard and keeps scores.
 function gameboard_reset() {
     // reset game but keep scores
     vsAi = true;
@@ -156,6 +166,7 @@ function gameboard_reset() {
     setBoard()
  }
 
+ // checks to see if the current player has won, or if their is a tie.
  function checkWinCondition() {
     // check if there are three of the same symbol in a row anywhere
     console.log("checking win conditions")
@@ -208,7 +219,7 @@ function gameboard_reset() {
     }
     return false;
 }
-
+// runs if someone passed the win condition check.
 function someoneWon(winner, tie) {
     // flip board
     gameboard.classList.add('winnerwinner');
@@ -216,7 +227,6 @@ function someoneWon(winner, tie) {
 
     //update score of player that one, remove remaining event listeners.
     if (tie) {
-        // change text to tie!
         turn_status.innerHTML = 'It\'\s a tie!';
     } else if (turnCounter % 2 === 0) {
         turn_status.innerHTML = 'The Ai wins!';
