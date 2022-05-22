e583cac4de659ee68a78201e96a564135c79711c
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-22 13:25:45 -0400

    added funtion to determine whos turn it is and mark the box

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log("hello");
-
+// list of my variables
 const blocks = document.querySelectorAll(".block");
 const playerX = 'X';
 const player0 = '0';
@@ -7,3 +7,38 @@ const newGame = document.getElementById("new-game-btn");
 
 console.log(blocks)
 const gameBoard = Array(blocks.length);
+
+//create a function to determine which turn it is and allow that turn to click a square 
+//if square is already marked then return 
+let turn = playerX
+
+function markBoard(event){
+    
+    let block = event.target;
+    
+    if(block.innerText != ""){
+        return;
+    }
+    else if (turn === playerX) {
+        block.innerText = playerX;
+        turn = player0;
+    }
+    else {
+        block.innerText = player0;
+        turn = playerX;
+    }
+}
+
+blocks.forEach((event) => {
+    event.addEventListener("click", markBoard)
+});
+
+//create an event listener for each square
+
+
+//create list of all winning combinations
+//have a function to check for a winning combo on the board after each turn
+
+//create a draw function
+
+// lastly make the reset button erase the board and start over
