e0728e7cb249eada132fffed1d5cd8aaac067885
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-22 12:49:22 -0400

    listed my constants and listed the steps needed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("hello");
+
+const blocks = document.querySelectorAll(".block");
+const playerX = 'X';
+const player0 = '0';
+const newGame = document.getElementById("new-game-btn");
+
+console.log(blocks)
+const gameBoard = Array(blocks.length);
