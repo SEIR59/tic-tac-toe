8da1fb211007d0d79bf7a2634029abc955460511
christopher chapa <chapachristopher16@gmail.com>
2022-05-26 13:21:13 -0500

    update app.js to add classes to the buttons selected by players

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ let currentPlayer = () => {
     }
 }
 
-
+// Function to disable all buttons in the case of win and to disable buttons after being pressed
 const clickDisabled = () => {
     document.getElementById('1').disabled = true
     document.getElementById('2').disabled = true
@@ -36,7 +36,9 @@ const clickDisabled = () => {
     document.getElementById('9').disabled = true
 }
 
-let count = 0
+let count = 0 // Count to keep track of game if 9 turns have been played the and no win condition is met the game will end in a tie
+
+// Board logic to check for win conditions by splicing letters into an array and checking if they match in the order of the possible win combinations
 const winConditions = () => {
     if (gameBoard[0] !== '' && gameBoard[1] === gameBoard[0] && gameBoard[2] === gameBoard[1]) {
         winMessage()
@@ -66,23 +68,26 @@ const winConditions = () => {
        document.getElementById('winMessageText').innerText = 'The game has ended in a tie'
     }
 }
-
+// Function to switch players to display correct winner with winning message
 winMessage = function () {
     document.getElementById('winMessageText').innerText = `player ${currentPlayer(playerTurn)} is the winner`
 }
-
+// access html to dynamically change the text of whose turn it is
 document.getElementById('whichTurn').innerText = playerTurn
 
+// Button one logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('1').addEventListener('click', () => {
     if (document.getElementById('1-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('1-text').innerText = 'X'
             gameBoard.splice(0,1,'X')
+            document.getElementById('1-text').setAttribute('class', 'x')
             currentPlayer()
             console.log(gameBoard)
         }else if(playerTurn === 2) {
             document.getElementById('1-text').innerText = 'O'
             gameBoard.splice(0,1,'O')
+            document.getElementById('1-text').setAttribute('class', 'O')
             currentPlayer()
             console.log(gameBoard)
 
@@ -94,17 +99,19 @@ document.getElementById('1').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
-
+// Button two logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('2').addEventListener('click', () => {
     if (document.getElementById('2-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('2-text').innerText = 'X'
+            document.getElementById('2-text').setAttribute('class', 'x')
             gameBoard.splice(1,1,'X')
             currentPlayer()
             console.log(gameBoard)
 
         }else if(playerTurn === 2) {
             document.getElementById('2-text').innerText = 'O'
+            document.getElementById('2-text').setAttribute('class', 'O')
             gameBoard.splice(1,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -117,11 +124,12 @@ document.getElementById('2').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
-
+// Button 3 logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('3').addEventListener('click', () => {
     if (document.getElementById('3-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('3-text').innerText = 'X'
+            document.getElementById('3-text').setAttribute('class', 'x')
             gameBoard.splice(2,1,'X')
             currentPlayer()
             console.log(gameBoard)
@@ -129,6 +137,7 @@ document.getElementById('3').addEventListener('click', () => {
             
         }else if(playerTurn === 2) {
             document.getElementById('3-text').innerText = 'O'
+            document.getElementById('3-text').setAttribute('class', 'O')
             gameBoard.splice(2,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -141,11 +150,12 @@ document.getElementById('3').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
-
+// Button four logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('4').addEventListener('click', () => {
     if (document.getElementById('4-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('4-text').innerText = 'X'
+            document.getElementById('4-text').setAttribute('class', 'x')
             gameBoard.splice(3,1,'X')
             currentPlayer()
             console.log(gameBoard)
@@ -153,6 +163,7 @@ document.getElementById('4').addEventListener('click', () => {
             
         }else if(playerTurn === 2) {
             document.getElementById('4-text').innerText = 'O'
+            document.getElementById('4-text').setAttribute('class', 'O')
             gameBoard.splice(3,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -165,17 +176,19 @@ document.getElementById('4').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
-
+// Button five logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('5').addEventListener('click', () => {
     if (document.getElementById('5-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('5-text').innerText = 'X'
+            document.getElementById('5-text').setAttribute('class', 'x')
             gameBoard.splice(4,1,'X')
             currentPlayer()
             console.log(gameBoard)
 
         }else if(playerTurn === 2) {
             document.getElementById('5-text').innerText = 'O'
+            document.getElementById('5-text').setAttribute('class', 'O')
             gameBoard.splice(4,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -188,11 +201,12 @@ document.getElementById('5').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
-
+// Button six logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('6').addEventListener('click', () => {
     if (document.getElementById('6-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('6-text').innerText = 'X'
+            document.getElementById('6-text').setAttribute('class', 'x')
             gameBoard.splice(5,1,'X')
             currentPlayer()
             console.log(gameBoard)
@@ -200,6 +214,7 @@ document.getElementById('6').addEventListener('click', () => {
             
         }else if(playerTurn === 2) {
             document.getElementById('6-text').innerText = 'O'
+            document.getElementById('6-text').setAttribute('class', 'O')
             gameBoard.splice(5,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -212,16 +227,19 @@ document.getElementById('6').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
+// Button seven logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('7').addEventListener('click', () => {
     if (document.getElementById('7-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('7-text').innerText = 'X'
+            document.getElementById('7-text').setAttribute('class', 'x')
             gameBoard.splice(6,1,'X')
             currentPlayer()
             console.log(gameBoard)
 
         }else if(playerTurn === 2) {
             document.getElementById('7-text').innerText = 'O'
+            document.getElementById('7-text').setAttribute('class', 'O')
             gameBoard.splice(6,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -234,10 +252,12 @@ document.getElementById('7').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
+// Button eight logic and conditions add text, edit class, whose turn, switch turns after player chooses button
 document.getElementById('8').addEventListener('click', () => {
     if (document.getElementById('8-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('8-text').innerText = 'X'
+            document.getElementById('8-text').setAttribute('class', 'x')
             gameBoard.splice(7,1,'X')
             currentPlayer()
             console.log(gameBoard)
@@ -245,6 +265,7 @@ document.getElementById('8').addEventListener('click', () => {
             
         }else if(playerTurn === 2) {
             document.getElementById('8-text').innerText = 'O'
+            document.getElementById('8-text').setAttribute('class', 'O')
             gameBoard.splice(7,1,'O')
             currentPlayer()
             console.log(gameBoard)
@@ -257,10 +278,12 @@ document.getElementById('8').addEventListener('click', () => {
     winConditions()
     document.getElementById('whichTurn').innerText = playerTurn
 })
+// Button nine logic and conditions add text, edit class, whose turn, switch turns after player chooses buttons
 document.getElementById('9').addEventListener('click', () => {
     if (document.getElementById('9-text').innerText == '') {
         if (playerTurn === 1) {
             document.getElementById('9-text').innerText = 'X'
+            document.getElementById('9-text').setAttribute('class', 'x')
             gameBoard.splice(8,1,'X')
             currentPlayer()
             console.log(gameBoard)
@@ -268,6 +291,7 @@ document.getElementById('9').addEventListener('click', () => {
             
         }else if(playerTurn === 2) {
             document.getElementById('9-text').innerText = 'O'
+            document.getElementById('9-text').setAttribute('class', 'O')
             gameBoard.splice(8,1,'O')
             currentPlayer()
             console.log(gameBoard)
