0516c895befac06a1e6a3bd78b5742c90aaa974e
mkhan42 <michallekhan9@gmail.com>
2022-05-31 21:40:59 -0400

    fixed game

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-let playerOneVal = new Array()
-let playerTwoVal = new Array();
+let playerOneVal = []
+let playerTwoVal = []
 
 const winsArr = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 let winConditon = false
 
 let currentTurn = 'player1';
 
+let state = false;
+
 
 const gridClass = document.querySelectorAll('.grid-class');
 // const btn = document.querySelector('.btn');
+const boxes = document.querySelectorAll('#box')
 
 const currPlay = document.querySelector('#player-turn');
 
+let moves = 0;
+
+let winner;
 
 
 let checkForWinner = (playerArray) => {
@@ -21,7 +27,7 @@ let checkForWinner = (playerArray) => {
         console.log(winsArr[i], "WIN CONDITION")
         //.every checks if playerVal is in winsArr// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
          isWinner =  winsArr[i].every(playerVal => {
-            console.log(winsArr[i].includes(playerVal), "FINAL")
+            //console.log(winsArr[i].includes(playerVal), "FINAL")
             return playerArray.includes(playerVal)
         })
         if(isWinner) break;
@@ -30,34 +36,65 @@ let checkForWinner = (playerArray) => {
 }
 
 
-function playGame() {
+//console.log(box);
 
+// let endGame = () => {
+
+//     for(let i = 0; i < 9; i++) {
+//             box[i].style.pointerEvents = 'none';
+//         }
+// }
+
+// function checkPlayerWin() {
+//     const isPlayerOneWinner = checkForWinner(playerOneVal);
+//     const isPlayerTwoWinner = checkForWinner(playerTwoVal);
+
+//     if(isPlayerOneWinner){
+//         winner = isPlayerOneWinner;
+//         currPlay.textContent = "Player One Wins"
+//         endGame();
+//     } 
+//     else if (isPlayerTwoWinner){
+//         winner = isPlayerOneWinner;
+//         currPlay.textContent = "Player Two Wins"
+//         endGame();
+//     }
+//     //state = true
+// }
+
+
+
+function playGame() {
+    //if(state) {
     for(let i = 0; i < 9; i++){
         gridClass[i].addEventListener('click', (event) => {
             if(currentTurn === 'player1'){
-                gridClass[i].innerHTML = "X"
+                gridClass[i].textContent = "X"
                 playerOneVal.push(i);
                 gridClass[i].style.pointerEvents = 'none';
                 moves++;
                 currentTurn = 'player2';
-                currPlay.innerHTML = "Player Two's Turn"
+                currPlay.textContent = "Player Two's Turn"
                 if(moves === 9) {
-                    currPlay.innerHTML = "Cat's Game"
+                    currPlay.textContent = "Cat's Game"
                 }
             } else if(currentTurn === 'player2'){
-                gridClass[i].innerHTML = "O";
+                gridClass[i].textContent = "O";
                 playerTwoVal.push(i);
                 gridClass[i].style.pointerEvents = 'none';
                 moves++;
                 currentTurn = 'player1'
-                currPlay.innerHTML = "Player One's Turn"
+                currPlay.textContent = "Player One's Turn"
                 if(moves === 9) {
-                    currPlay.innerHTML = "Cat's Game"
+                    currPlay.textContent = "Cat's Game"
                 }
             }
-            checkPlayerWin();
-        })  
+                checkPlayerWin();
+                endGame();
+
+        }) 
     }
+//}
 }
 
 function checkPlayerWin() {
@@ -65,21 +102,30 @@ function checkPlayerWin() {
     const isPlayerTwoWinner = checkForWinner(playerTwoVal);
 
     if(isPlayerOneWinner){
-        currPlay.innerHTML = "Player One Wins"
+        currPlay.textContent = "Player One Wins"
+        state = true;
     } 
-    else if(isPlayerTwoWinner){
-        currPlay.innerHTML = "Player Two Wins"
+    else if (isPlayerTwoWinner){
+        currPlay.textContent = "Player Two Wins"
+        state = true
     }
-
-    // gridClass.style.pointerEvents = 'none';
-
 }
+
+
+let endGame = () => {
+
+    for(let i = 0; i < 9; i++) {
+        if(state === true)
+            gridClass[i].style.pointerEvents = 'none';
+        }
+}
+
 
 
 // let clicked = false;
 
-//clear interval
-//stop interval
+// clear interval
+// stop interval
 
 
 // function reset() {
@@ -97,7 +143,7 @@ function checkPlayerWin() {
 //     }
 // }
 
-playGame();
-reset();
+playGame()
+// reset();
 
 
